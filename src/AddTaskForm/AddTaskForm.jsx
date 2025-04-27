d43bf653a9bfa63151770de1dacf8e6f75af7089
max gromov <maximgromov@gmail.com>
2025-04-27 17:20:18 +0300

    Add component AddTaskForm

---
+++ src/AddTaskForm/AddTaskForm.jsx
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+
+class AddTaskForm extends Component {
+
+    constructor(props){
+        super(props);
+        this.state = {
+            task: ''
+        };
+    }
+
+    taskChanged(event){
+        this.setState({
+            task: event.target.value
+        });
+    }
+
+    submit(event){
+        event.preventDefault();
+    }
+
+    render() {
+        return (
+            <form onSubmit={this.submit.bind(this)}>
+                <input  value={this.state.task} 
+                onChange={this.taskChanged.bind(this)}/>
+                <button>Add Task</button>
+            </form>
+        );
+    }
+}
+
+export default AddTaskForm;
--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
+import { Component } from 'react';
 import './App.css'
 import MyForm from './MyForm/MyForm';
 import MyMap from './MyMap/MyMap'
+import AddTaskForm from './AddTaskForm/AddTaskForm';
 
-function App() {
+class App extends Component() {
 
-  return (
-    <>
-      <div>
-        <MyForm/>
+  render(){
+    return (
+      <div className='App'>
+        <AddTaskForm/>
       </div>
-    </>
   );
+  }
 }
 
 export default App
